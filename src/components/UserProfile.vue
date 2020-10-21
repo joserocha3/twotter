7a4687f6e2481067b4d7c73a5e9b5d17d29a2fc9
Pablo Rocha <jose.rocha3@gmail.com>
2020-10-21 00:42:13 -0500

    setup twoots

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div v-if="user.isAdmin" class="user-profile__admin-badge">
+        Admin
+      </div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favorite="toggleFavorite"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TwootItem from './TwootItem'
+
+export default {
+  name: 'App',
+  components: { TwootItem },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_PabloRocha',
+        firstName: 'Pablo',
+        lastName: 'Rocha',
+        email: 'pablo@pablorocha.me',
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: 'Twotter is Amazing!' },
+          {
+            id: 2,
+            content: "Don't forget to read my blog!",
+          },
+        ],
+      },
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    },
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower`)
+      }
+    },
+  },
+  mounted() {
+    this.followUser()
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    },
+    toggleFavorite(id) {
+      console.log(`Favorited tweet ${id}`)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 50px;
+  padding: 50px 5%;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+  margin-bottom: auto;
+}
+
+.user-profile__twoots-wrapper {
+  display: grid;
+  grid-gap: 10px;
+  margin-bottom: auto;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
